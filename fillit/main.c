24db0eaf07a08d5ac3a6d882a805a52e47fb0877
komil4 <kam.haj1997@gmail.com>
2019-11-27 11:02:38 +0300

    test2

--- fillit/main.c
+++ fillit/main.c
@@ -16,14 +16,12 @@ int main(int argc, char **argv)
 {
 	t_element *element;
 
-
-	
-
  	element = NULL;
 	if (argc != 2)
 		return(-1);
 	element = create_elements(argv[1]);
 	if (element != NULL)
+	//Hello world
 	//printf("Elements created!");
 	//print_elements(element);
 	//printf("Elements created!");
